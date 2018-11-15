fad0f24346f71642adf77489a99d1f293ea3684d
ocgabs <39992236+ocgabs@users.noreply.github.com>
2018-11-15 14:45:32 +0000

    Add files via upload
    LTRANS modified to work with NEMO 
    Vtransform = 5 in the input file LTRANS.data was added to activate it. 
    Main changes implied that LTRANS understands the vertical grid of NEMO. 
    The following routines were modified:
    parameter_module.f90
    LTRANS.f90
    hydrodynamic_module.f90
    Also a Lophelia bahaviour was added Behavior = 8 on  LTRANS.data activates it. 
    Code changes on behaviour_module.f90

---
+++ getWlevels.f90
@@ -0,0 +1,23 @@
+  CASE(5) !NEMO depths from e3t=T-cell thickness
+           usv=bathy(ele,1:4) !ISSUE!!!!
+            mus= MAXVAL(usv)
+
+        do b=1,mus
+	    E30 = getInterp("E3T0e",b) !NEMO
+ 	    E30w = getInterp("E3W0e",b)
+ 	    E3T(b) = E30*(1.0 + zeta/h) 
+            E3W(b) = E30w*( 1.0 + zeta/h )
+	enddo
+
+            dept(1)=0.5*E3W(1)
+            depw(1)=0.0
+          
+           do a=2,mus
+              dept(a) = dept(a-1) + E3W(a)
+              depw(a) = depw(a-1) + E3T(a-1)
+	   enddo
+	do a=1,mus !Flip over NEMO
+        fdepw(a)=depw((mus+1)-a)
+	enddo
+     
+        getWlevel = DBLE(-1.0) * fdepw(i)
